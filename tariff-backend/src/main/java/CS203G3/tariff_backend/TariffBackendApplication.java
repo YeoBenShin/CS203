e2dc36681ed11298ec58f2fa13b7a844e16390fe
weich00n <weic.chong.2024@computing.smu.edu.sg>
2025-09-02 23:16:35 +0800

    Initialize SpringBoot Backend
    included swaggerui dependency
    
    #TODO
    1) finalize database
    2) Authentication Method

---
+++ tariff-backend/src/main/java/CS203G3/tariff_backend/TariffBackendApplication.java
@@ -0,0 +1,13 @@
+package CS203G3.tariff_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TariffBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TariffBackendApplication.class, args);
+	}
+
+}
---
+++ tariff-backend/src/test/java/CS203G3/tariff_backend/TariffBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package CS203G3.tariff_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TariffBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
