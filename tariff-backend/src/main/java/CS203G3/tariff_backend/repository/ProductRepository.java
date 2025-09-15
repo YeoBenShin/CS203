a610f51d5296baca24e1ed6e7bf17e4219cfef71
weich00n <weic.chong.2024@computing.smu.edu.sg>
2025-09-15 19:25:17 +0800

    feat: product backend routes
    Implemented model, repo, service, controller for crud

---
+++ tariff-backend/src/main/java/CS203G3/tariff_backend/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package CS203G3.tariff_backend.repository;
+
+import CS203G3.tariff_backend.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+    // Additional custom query methods can be added here if needed
+}
