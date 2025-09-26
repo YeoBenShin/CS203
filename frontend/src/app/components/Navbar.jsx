970938bc1db774769d536dbe55e77bc3040b8931
YeoBenShin <orang3cats@gmail.com>
2025-09-26 13:46:51 +0800

    feat: created sepearate navbar for user and admin refactor: changed view-tariffs route to user route (not admin anymore)

--- frontend/src/app/components/Navbar.jsx
+++ frontend/src/app/components/Navbar.jsx
@@ -2,41 +2,74 @@
 import Link from "next/link";
 import React, { useState, useRef } from "react";
 import { UserButton } from "@clerk/nextjs";
-import { User } from "@clerk/nextjs/server";
+import { useUser } from "@clerk/nextjs";
 
 export default function Navbar() {
-  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [adminDropDown, setAdminDropDown] = useState(false);
+  const [userDropDown, setUserDropDown] = useState(false);
   const timeoutRef = useRef();
+  const { user } = useUser();
+  const role = user?.publicMetadata?.role;
 
-  const handleMouseEnter = () => {
+  const handleUserMouseEnter = () => {
     clearTimeout(timeoutRef.current);
-    setDropdownOpen(true);
+    setUserDropDown(true);
   };
-  const handleMouseLeave = () => {
-    timeoutRef.current = setTimeout(() => setDropdownOpen(false), 200);
+  const handleUserMouseLeave = () => {
+    timeoutRef.current = setTimeout(() => setUserDropDown(false), 200);
+  };
+
+  const handleAdminMouseEnter = () => {
+    clearTimeout(timeoutRef.current);
+    setAdminDropDown(true);
+  };
+  const handleAdminMouseLeave = () => {
+    timeoutRef.current = setTimeout(() => setAdminDropDown(false), 200);
   };
 
   return (
     <nav className="w-full bg-white shadow-md border-b border-gray-200 p-6 flex items-center justify-between">
       <Link href="/" className="text-3xl font-bold text-black">TARRIFY</Link>
       <ul className="flex space-x-6 text-sm font-medium">
-        <li
-          className="relative group"
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-        >
-          <button className="text-black text-lg hover:text-blue-600 focus:outline-none">Admin Page</button>
-          <div
-            className={`absolute right-0 mt-2 w-56 bg-white border border-gray-200 rounded shadow-lg z-50 transition-opacity duration-200 ${dropdownOpen ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"}`}
+      {role === "user" ? (
+          <li
+            className="relative group"
+            onMouseEnter={handleUserMouseEnter}
+            onMouseLeave={handleUserMouseLeave}
           >
-            <Link href="/admin" className="block px-4 py-2 text-black hover:bg-blue-100">Create Tariff</Link>
-            <Link href="/admin/product" className="block px-4 py-2 text-black hover:bg-blue-100">Add Product</Link>
-            <Link href="/admin/view-tariffs" className="block px-4 py-2 text-black hover:bg-blue-100">View All Tariffs</Link>
-            <Link href="/admin/view-mappings" className="block px-4 py-2 text-black hover:bg-blue-100">View All Mappings</Link>
-          </div>
-        </li>
+
+            <button className="text-black text-lg hover:text-blue-600 focus:outline-none">Menu</button>
+            <div
+              className={`absolute right-0 mt-2 w-56 bg-white border border-gray-200 rounded shadow-lg z-50 transition-opacity duration-200 ${userDropDown ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"}`}
+            >
+              <Link href="/" className="block px-4 py-2 text-black hover:bg-blue-100">Home</Link>
+              <Link href="/calculator" className="block px-4 py-2 text-black hover:bg-blue-100">Tariff Calculator</Link>
+              <Link href="view-tariffs" className="block px-4 py-2 text-black hover:bg-blue-100">View All Tariffs</Link>
+            </div>
+          </li>
+        ) : <div></div>}
+
+        {role === "admin" ? (
+          <li
+            className="relative group"
+            onMouseEnter={handleAdminMouseEnter}
+            onMouseLeave={handleAdminMouseLeave}
+          >
+
+            <button className="text-black text-lg hover:text-blue-600 focus:outline-none">Menu</button>
+            <div
+              className={`absolute right-0 mt-2 w-56 bg-white border border-gray-200 rounded shadow-lg z-50 transition-opacity duration-200 ${adminDropDown ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"}`}
+            >
+              <Link href="/" className="block px-4 py-2 text-black hover:bg-blue-100">Home</Link>
+              <Link href="/calculator" className="block px-4 py-2 text-black hover:bg-blue-100">Tariff Calculator</Link>
+              <Link href="/admin" className="block px-4 py-2 text-black hover:bg-blue-100">Create Tariff</Link>
+              <Link href="/admin/product" className="block px-4 py-2 text-black hover:bg-blue-100">Add Product</Link>
+              <Link href="view-tariffs" className="block px-4 py-2 text-black hover:bg-blue-100">View All Tariffs</Link>
+              <Link href="/admin/view-mappings" className="block px-4 py-2 text-black hover:bg-blue-100">View All Mappings</Link>
+            </div>
+          </li>
+        ) : <div></div>}
         <li>
-          {/* <Link href="#" className="text-black text-lg hover:text-blue-600">About Us</Link> */}
           <UserButton showName
             appearance={{
               elements: {
