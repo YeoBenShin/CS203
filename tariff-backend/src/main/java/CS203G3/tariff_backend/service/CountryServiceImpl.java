5f733a1e86d38262d6b46c481c9405ead1cfb729
weich00n <weic.chong.2024@computing.smu.edu.sg>
2025-09-28 12:42:40 +0800

    backend: country routes

---
+++ tariff-backend/src/main/java/CS203G3/tariff_backend/service/CountryServiceImpl.java
@@ -0,0 +1,59 @@
+package CS203G3.tariff_backend.service;
+
+import CS203G3.tariff_backend.model.Country;
+import CS203G3.tariff_backend.repository.CountryRepository;
+import CS203G3.tariff_backend.exception.ResourceNotFoundException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+public class CountryServiceImpl implements CountryService {
+    
+    private final CountryRepository countryRepository;     
+
+    public CountryServiceImpl(CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
+    }
+
+    @Override
+    @Transactional
+    public Country createCountry(Country country) {
+        return countryRepository.save(country);
+    }
+
+    @Override
+    @Transactional
+    public void deleteCountry(String id) {
+        if (!countryRepository.existsById(id)) {
+            throw new ResourceNotFoundException("Country", id);
+        }
+        countryRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Country> getAllCountries() {
+    
+        return countryRepository.findAll();
+    }
+
+    @Override
+    public Country getCountryById(String id) {
+        return countryRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Country", id));
+    }
+
+    @Override
+    @Transactional
+    public Country updateCountry(String id, Country country) {
+        return countryRepository.findById(id)
+            .map(existingCountry -> {
+                existingCountry.setName(country.getName());
+                existingCountry.setRegion(country.getRegion());
+                return countryRepository.save(existingCountry);
+            })
+            .orElseThrow(() -> new ResourceNotFoundException("Country", id));
+    }
+}
