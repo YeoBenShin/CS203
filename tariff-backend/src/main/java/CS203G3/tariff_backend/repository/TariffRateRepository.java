e9321ed7cb62f5b8fd238ff2ac359eae7be216e6
TrevorOYS <158175412+TrevorOYS@users.noreply.github.com>
2025-09-29 11:20:13 +0800

    DB: custom queries for retrieving tariff data

--- tariff-backend/src/main/java/CS203G3/tariff_backend/repository/TariffRateRepository.java
+++ tariff-backend/src/main/java/CS203G3/tariff_backend/repository/TariffRateRepository.java
@@ -4,7 +4,9 @@ import CS203G3.tariff_backend.model.TariffRate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TariffRateRepository extends JpaRepository<TariffRate, Long> {
-    
+    List<TariffRate> findAllByTariff_TariffID(Long tariffId);
 }
--- tariff-backend/src/main/java/CS203G3/tariff_backend/repository/TariffRepository.java
+++ tariff-backend/src/main/java/CS203G3/tariff_backend/repository/TariffRepository.java
@@ -2,7 +2,12 @@ package CS203G3.tariff_backend.repository;
 
 import CS203G3.tariff_backend.model.Tariff;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.sql.Date;
+import java.util.Optional;
 
 @Repository
 public interface TariffRepository extends JpaRepository<Tariff, Long> {
@@ -37,4 +42,10 @@ public interface TariffRepository extends JpaRepository<Tariff, Long> {
     //     @Param("newEffectiveDate") Date newEffectiveDate,
     //     @Param("newExpiryDate") Date newExpiryDate
     // );
+    @Query("SELECT t FROM Tariff t WHERE t.product.hSCode = :hSCode AND t.exporter.isoCode = :exporter AND t.effectiveDate <= :tradeDate AND t.expiryDate >= :tradeDate")
+    Optional<Tariff> findValidTariff(
+        @Param("hSCode") int hSCode,
+        @Param("exporter") String exporter,
+        @Param("tradeDate") Date tradeDate
+    );
 }
