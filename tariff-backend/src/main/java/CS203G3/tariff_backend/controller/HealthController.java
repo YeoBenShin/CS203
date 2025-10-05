a22594c8ba6b3d27e482cdf0a5ca2f01eb13d67c
TrevorOYS <158175412+TrevorOYS@users.noreply.github.com>
2025-10-05 22:00:42 +0800

    Create HealthController.java

---
+++ tariff-backend/src/main/java/CS203G3/tariff_backend/controller/HealthController.java
@@ -0,0 +1,17 @@
+/*
+Load balancer health check
+*/
+package CS203G3.tariff_backend.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("OK");
+    }
+}
