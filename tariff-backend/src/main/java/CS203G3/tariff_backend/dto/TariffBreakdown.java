850486f580bbb94e65313fda5bb536d647023206
Vincent <vincentc361@gmail.com>
2025-10-21 00:11:08 +0800

    fix: added reference
    added reference variable so each tariff breakdown can call the reference

--- tariff-backend/src/main/java/CS203G3/tariff_backend/dto/TariffBreakdown.java
+++ tariff-backend/src/main/java/CS203G3/tariff_backend/dto/TariffBreakdown.java
@@ -1,18 +1,26 @@
 package CS203G3.tariff_backend.dto;
 
-import CS203G3.tariff_backend.model.UnitOfCalculation;
-
 import java.math.BigDecimal;
+
+import CS203G3.tariff_backend.model.UnitOfCalculation;
 
 public class TariffBreakdown {
     private UnitOfCalculation type;
     private BigDecimal tariffRate;
     private BigDecimal tariffCost;
+    private String reference;
     
     public TariffBreakdown(UnitOfCalculation type, BigDecimal tariffRate, BigDecimal tariffCost) {
         this.type = type;
         this.tariffRate = tariffRate;
         this.tariffCost = tariffCost;
+    }
+
+    public TariffBreakdown(UnitOfCalculation type, BigDecimal tariffRate, BigDecimal tariffCost, String reference) {
+        this.type = type;
+        this.tariffRate = tariffRate;
+        this.tariffCost = tariffCost;
+        this.reference = reference;
     }
 
     public UnitOfCalculation getType() {
@@ -39,5 +47,11 @@ public class TariffBreakdown {
         this.tariffCost = tariffCost;
     }
 
-    
+    public String getReference() {
+        return reference;
+    }
+
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
 }
