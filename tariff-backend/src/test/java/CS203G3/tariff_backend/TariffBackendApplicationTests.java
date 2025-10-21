00696fa9a4f4ec22bb106f2bb3bedf01fa18e671
TrevorOYS <158175412+TrevorOYS@users.noreply.github.com>
2025-10-21 23:32:31 +0800

    Revert "Update TariffBackendApplicationTests.java"
    This reverts commit b673e0e5349e381e5e0335ffe08572c7ac45f97f.

--- tariff-backend/src/test/java/CS203G3/tariff_backend/TariffBackendApplicationTests.java
+++ tariff-backend/src/test/java/CS203G3/tariff_backend/TariffBackendApplicationTests.java
@@ -2,15 +2,10 @@ package CS203G3.tariff_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
-
-import CS203G3.tariff_backend.repository.UserRepository;
 
 @SpringBootTest(classes = TariffBackendApplication.class)
 class TariffBackendApplicationTests {
 
-	@MockitoBean
-    private UserRepository userRepository;
 	@Test
 	void contextLoads() {
 	}
