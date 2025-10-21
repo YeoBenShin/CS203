831a60d2eccee838d68949959baa62c1fe4757f3
TrevorOYS <158175412+TrevorOYS@users.noreply.github.com>
2025-10-21 23:46:17 +0800

    Update TariffBackendApplicationTests.java

--- tariff-backend/src/test/java/CS203G3/tariff_backend/TariffBackendApplicationTests.java
+++ tariff-backend/src/test/java/CS203G3/tariff_backend/TariffBackendApplicationTests.java
@@ -2,10 +2,20 @@ package CS203G3.tariff_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import CS203G3.tariff_backend.repository.UserRepository;
 
 @SpringBootTest(classes = TariffBackendApplication.class)
+@TestPropertySource(properties = {
+    "clerk.jwk-set-uri=http://localhost:8080/jwks" 
+})
 class TariffBackendApplicationTests {
 
+	@MockitoBean
+	private UserRepository userRepository;
+	
 	@Test
 	void contextLoads() {
 	}
