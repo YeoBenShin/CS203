25c0859bd8ed1a587a3e67051b8820777d69f640
Zaccheus <zacs91258739@gmail.com>
2025-10-23 18:06:27 +0800

    fixed add tariff page

--- tariff-backend/src/main/java/CS203G3/tariff_backend/model/UnitOfCalculation.java
+++ tariff-backend/src/main/java/CS203G3/tariff_backend/model/UnitOfCalculation.java
@@ -1,5 +1,7 @@
 package CS203G3.tariff_backend.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum UnitOfCalculation {
     BBL,
     C,
@@ -82,5 +84,12 @@ public enum UnitOfCalculation {
     W,
     WTS,
     X,
-    AV // Ad Valorem
+    AV; // Ad Valorem
+
+    @JsonCreator
+    public static UnitOfCalculation fromString(String key) {
+        if (key == null) return null;
+        return UnitOfCalculation.valueOf(key.toUpperCase());
+    }
+    
 }
